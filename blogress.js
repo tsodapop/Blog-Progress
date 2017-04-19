3b8a6bce34dd9cd265a2562b0b445c3d97aa8ded
tsodapop <jonathantso93@gmail.com>
2017-04-18 23:27:58 -0500

    Day 20

--- blogress.js
+++ blogress.js
@@ -2,8 +2,35 @@ $(document).ready(function(){
     $('#tabTwo').click(function(){
         $('body').animate({
             scrollTop: $("#tabPage").offset().top
-            });
+            },1000);
+        $('.one').hide();
         $('.two').show();
+        $('.three').hide();
+        $('.linkButton').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#tabOne').click(function(){
+        $('body').animate({
+            scrollTop: $("#tabPage").offset().top
+            },1000);
+        $('.one').show();
+        $('.two').hide();
+        $('.three').hide();
+        $('.linkButton').show();
+    });
+});
+
+$(document).ready(function(){
+    $('#tabThree').click(function(){
+        $('body').animate({
+            scrollTop: $("#tabPage").offset().top
+            },1000);
+        $('.one').hide();
+        $('.two').hide();
+        $('.three').show();
+        $('.linkButton').hide();
     });
 });
 
@@ -11,6 +38,6 @@ $(document).ready(function() {
 	$('#homeButton').click(function() {
 		$('body').animate({
 	            scrollTop: $("#Introduction").offset().top
-	            });
+	            },1000);
 		});
 });
